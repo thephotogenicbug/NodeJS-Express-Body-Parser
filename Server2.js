494677294edb427c53b7d329d5280708b7a67a02
thephotogenicbug <navinkumarachari@gmail.com>
2021-07-03 20:25:30 +0530

    NodeJS-Express-Body-Parser : 03-07-2021 : Fetch Employee details from Frontend and push it to MySQL

--- Server2.js
+++ Server2.js
@@ -1,42 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
+app.use(express.json());  // to parse the body while receiving a request from client
 
-// MySQL connection
-const mysql = require('mysql');
+// mysql connection 
+const mysql = require("mysql");
 const mydatabase = mysql.createConnection({
-     host: 'localhost',
-     user:'root',
-     password:'',
-     database:'reactone'
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'reactone'
 });
-mydatabase.connect(); // nodejs will get connected to mysql engine and to reactone database
+mydatabase.connect(); // nodejs will get connected to mysql engine and to reactone databse
 
-app.get('/booklist', function(req,res){ // req , res are called parameter
-mydatabase.query('select * from book', function(error, rows, fields){ // fields number of columns in table
-     if(error) throw error
-     res.send(JSON.stringify(rows));
-     res.end();
-})
-}) 
 
-app.get('/citylist', function(req,res){ // req , res are called parameter
-mydatabase.query('select * from city', function(error, rows, fields){ // fields number of columns in table
-     if(error) throw error
-     res.send(JSON.stringify(rows));
-     res.end();
-})
-}) 
-
-app.get('/productlist', function(req,res){ // req , res are called parameter
-mydatabase.query('select * from product', function(error, rows, fields){ // fields number of columns in table
-     if(error) throw error
-     res.send(JSON.stringify(rows));
-     res.end();
-})
+app.get("/booklist" , function(req , res){
+    mydatabase.query('select * from book' , function(error , rows, fields){
+        if(error) throw error
+        res.send( JSON.stringify(rows) );
+        res.end();
+    })
 })
 
-app.listen(5556, function(){
 
+app.post("/saveemp", function(req , res){
+    var employeeName = req.body.empname;
+    var empsalary = req.body.empsal;
+    var email = req.body.emailid;
+    var mobile = req.body.mobileno;
+    var city = req.body.cityname;
+    var sql = "insert into emp(name , salary, email , mobile , city) values('"+employeeName+"', '"+empsalary+"', '"+email+"', '"+mobile+"', '"+city+"')";
+    mydatabase.query( sql , function(error , rows, fields){
+        if(error) throw error
+        res.send("Employee Details Received Successfully !");
+        res.end();
+    })
+});
+
+
+app.get("/emplist" , function(req , res){
+    mydatabase.query('select * from emp order by empid desc' , function(error , rows, fields){
+        if(error) throw error
+        res.send( JSON.stringify(rows) );
+        res.end();
+    })
 })
+
+
+
+app.listen(2222, function(){
+    //   http://localhost:2222/booklist
+});
